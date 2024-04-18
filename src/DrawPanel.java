2f1fa0780c63676cd552551773c2fafc56a37f0c
BT_4N2_10 <ur@mom.com>
2024-04-18 13:55:51 -0400

    Made the start screen! All I have to do now is figure out how to make the system register button presses

--- src/DrawPanel.java
+++ src/DrawPanel.java
@@ -1,10 +1,10 @@
+import java.awt.*;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 import javax.swing.JPanel;
-import java.awt.Graphics;
-import java.awt.Point;
+import java.util.Scanner;
 import java.util.ArrayList;
-import java.awt.Font;
+
 public class DrawPanel extends JPanel implements KeyListener {
     public DrawPanel() {
         this.addKeyListener(this);
@@ -12,9 +12,31 @@ public class DrawPanel extends JPanel implements KeyListener {
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
+        Scanner s = new Scanner(System.in);
+        intro(g);
+        //String enter = s.nextLine();
+        //int enterEnter = Integer.parseInt(enter);
+        //if (enterEnter == KeyEvent.VK_ENTER) {
+        //    signInScreen(g);
+        //}
     }
 
+    public void intro(Graphics g) {
+        g.setColor(new Color(0, 0, 0));
+        g.fillRect(0, 0, 500, 500);
+        g.setColor(new Color(255, 255, 255));
+        g.setFont(new Font("Impact", Font.BOLD, 25));
+        g.drawString("YOU DON'T KNOW", 150, 100);
+        g.setFont(new Font("Impact", Font.BOLD, 90));
+        g.drawString("JACK", 145, 180);
+        g.setFont(new Font("Monospaced", Font.PLAIN, 15));
+        g.drawString("Press Enter to continue", 135, 300);
+    }
+    public void signInScreen(Graphics g) {
+        g.setColor(new Color(100));
+        g.fillRect(0, 0, 500, 500);
+        g.setColor(new Color(255, 165, 0));
+    }
     public void keyTyped(KeyEvent e) {
 
     }
--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ import javax.sound.sampled.*;
 public class Main {
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         Scanner s = new Scanner(System.in);
+        TheAction theGame = new TheAction("The GAME ");
+        /**
         System.out.println("What's your name?");
         System.out.print("(Type in your name): ");
         String playerName = s.nextLine();
@@ -15,13 +17,13 @@ public class Main {
         if (playerName.equals("Cookie") || playerName.equals("Cookie Masterson")) {
             System.out.println("Heyyy!! Cookie actually showed up! Sorry I can't host as good as you, but hope you enjoy the game regardless!");
         }
-        System.out.println(cookieNotCookie.getName() + ":");
-        System.out.println("$" + cookieNotCookie.getMoney());
+        System.out.println(cookieNotCookie.printPlayer());
         MultipleChoiceQuestion test = new MultipleChoiceQuestion("Miku Miku ni shite Ageru\nHow many words are in the snippet of that song?1. Three2. Four3. Five4. 39", "3", 1);
         System.out.println(test.printMultipleChoice());
         System.out.print("Type the number choice: ");
         String playerChoice = s.nextLine();
         test.checkQuestion(cookieNotCookie, playerChoice);
         System.out.println(cookieNotCookie.printPlayer());
+         **/
     }
 }
